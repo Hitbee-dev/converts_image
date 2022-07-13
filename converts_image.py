aaa05d6b45821ad0045412e9f4a33b494b011b60
blpeng2 <blpeng@naver.com>
2022-07-14 02:00:47 +0900

    프로그램 완성

--- converts_image.py
+++ converts_image.py
@@ -1,10 +1,54 @@
 from PIL import Image
-import os
+import sys
+from PyQt5.QtWidgets import *
+import pillow_heif
 
-file_list = os.listdir('images/')
-print(file_list)
+class MyApp(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+        self.setAcceptDrops(True)
+        grid = QGridLayout()
+        self.setLayout(grid)
 
-for image in file_list:
-    imgname = image.split(".")
-    img = Image.open(f'images/{imgname[0]}.{imgname[1]}').convert('RGB')
-    img.save(f'converts_images/{imgname[0]}.png', 'png')
+        label1 = QLabel("HEIC파일을 드래그하세요.")
+        grid.addWidget(label1, 0, 0)
+
+    def dragEnterEvent(self, event):
+        if event.mimeData().hasUrls():
+            event.accept()
+        else:
+            event.ignore()
+
+    def dropEvent(self, event):
+        files = [u.toLocalFile() for u in event.mimeData().urls()]
+        for f in files:
+            print(f)
+            heif_file = pillow_heif.read_heif(f)
+            image = Image.frombytes(
+            heif_file.mode,
+            heif_file.size,
+            heif_file.data,
+            "raw",
+            )
+
+            image.save(f"./{f.split('/')[-1].split('.')[0]}.png", format("png"))
+
+    def center(self):
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
+
+    def initUI(self):
+        self.setWindowTitle('HEIC to PNG')
+        self.resize(400, 200)
+        self.center()
+        self.show()
+
+
+if __name__ == '__main__':
+   app = QApplication(sys.argv)
+   ex = MyApp()
+   ex.show()
+   sys.exit(app.exec_())
